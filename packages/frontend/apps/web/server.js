31650f2caac44b2f900f3a43589cc8dbc2c868ab
Jordan <97871694+MrDataJ@users.noreply.github.com>
2025-04-28 12:14:19 +0800

    Create server.js

---
+++ packages/frontend/apps/web/server.js
@@ -0,0 +1,17 @@
+// server.js
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve static files from dist folder
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// Catch-all to serve index.html for SPA routing
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Frontend running at http://localhost:${PORT}`);
+});
